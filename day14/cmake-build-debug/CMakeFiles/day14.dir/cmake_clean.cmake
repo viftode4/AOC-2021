318e64643966bec526a7a1109ba88c6885ee092a
levladutz <viftode4@yahoo.com>
2021-12-14 10:05:57 +0100

    day 14

---
+++ day14/cmake-build-debug/CMakeFiles/day14.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/day14.dir/main.cpp.obj"
+  "day14.exe"
+  "day14.exe.manifest"
+  "day14.pdb"
+  "libday14.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/day14.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
